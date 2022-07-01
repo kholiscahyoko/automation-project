7d71cee01ff0961ffdeb42e15f52a33e36a06c22
Kholis Cahyoko <kholis.cahyoko@gmail.com>
2022-07-02 01:36:59 +0700

    main py : uncomment goal id

--- main.py
+++ main.py
@@ -5,13 +5,13 @@ from classes.goal_id import GoalID
 from classes.tempo_co import TempoCo
 
 def main():
-    # #goal indonesia
-    # goal_id = GoalID()
-    # goal_id.process()
-    #
-    # with open("./data/goal_id.json", "w") as external_file:
-    #     external_file.write(json.dumps(goal_id.result))
-    #     external_file.close()
+    #goal indonesia
+    goal_id = GoalID()
+    goal_id.process()
+
+    with open("./data/goal_id.json", "w") as external_file:
+        external_file.write(json.dumps(goal_id.result))
+        external_file.close()
 
     #tempo.co
     tempo_co = TempoCo()
