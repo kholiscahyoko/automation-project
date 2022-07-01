9eda868e939da5689d44a4d14fca348954adf718
Kholis Cahyoko <kholis.cahyoko@gmail.com>
2022-07-02 01:35:05 +0700

    add tempo co

---
+++ classes/tempo_co.py
@@ -0,0 +1,33 @@
+from urllib.request import urlopen
+from lxml import etree
+
+class TempoCo:
+    def __init__(self):
+        self.url = "https://www.tempo.co"
+        self.htmlparser = etree.HTMLParser()
+        self.result = {
+            'url': self.url,
+            'data': []
+        }
+
+    def process(self):
+        response = urlopen(self.url)
+        tree = etree.parse(response, self.htmlparser)
+
+        for element in tree.xpath('//div[@class="card-box ft240 margin-bottom-sm"]'):
+            #media image
+            media_poster = element.xpath('.//figure/a/img')[0]
+            img_src = media_poster.get("src")
+
+            #media text and article url
+            media_content = element.xpath('.//article')[0]
+            article_title = media_content.xpath('.//h2/a')[0].text.strip()
+            article_url = media_content.xpath('.//h2/a')[0].get("href")
+            article_date = media_content.xpath('.//h4')[0].text.strip()
+            tags = []
+            self.result["data"].append({
+                "img_src" : img_src,
+                "article_date" : article_date,
+                "article_url" : article_url,
+                "article_title" : article_title
+            })
--- main.py
+++ main.py
@@ -2,13 +2,23 @@
 
 import json
 from classes.goal_id import GoalID
+from classes.tempo_co import TempoCo
 
 def main():
-    goal_id = GoalID()
-    goal_id.process()
+    # #goal indonesia
+    # goal_id = GoalID()
+    # goal_id.process()
+    #
+    # with open("./data/goal_id.json", "w") as external_file:
+    #     external_file.write(json.dumps(goal_id.result))
+    #     external_file.close()
 
-    with open("./data/goal_id.json", "w") as external_file:
-        external_file.write(json.dumps(goal_id.result))
+    #tempo.co
+    tempo_co = TempoCo()
+    tempo_co.process()
+
+    with open("./data/tempo_co.json", "w") as external_file:
+        external_file.write(json.dumps(tempo_co.result))
         external_file.close()
 
 main()
